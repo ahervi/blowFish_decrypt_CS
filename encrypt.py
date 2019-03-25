dd0dadfd22d01f880f983be9becd24d3b6225762
Antoine hervieu <hervieu.antoine@gmail.com>
2019-03-25 13:21:57 +0100

    renommage de variable et saut de 14 octets

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from Crypto.Cipher import Blowfish
 import sys
 from struct import pack
@@ -8,25 +7,25 @@ annee = np.int16(2019)
 mois = np.int16(3)
 jour = np.int16(5)
 jourSemaine = np.int16(3)
-heure = np.int16(9)
-#minute = np.int16(55)
+heure = np.int16(10)
+minute = np.int16(55)
 #seconde = np.int16(12)
 #ms
 bs = Blowfish.block_size
 iv =  b'@GPCODE\0'
 print("IV : " + str(iv))
 with open('index.html', 'rb') as f:
-    ciphertexte = f.read()
+    plaintext = f.read()
 
-ciphertexte = ciphertexte[14:]
+plaintext = plaintext[4:]
+print("Ciphertext nettoyé : " + str(plaintext))
 
-print("Ciphertext nettoyé : " + str(ciphertexte))
-
-plen = bs - len(ciphertexte) % bs
+#plaintext = b'\x00\x00\x00\x22\x4c\xd3\x3b\x9b\x61\x5a\xeb\x24\x6e\x70\xe4\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x18\x93\xae\x41\x80\x68\x9c\x9a\xbe\x49\x67\xc5\x0a'
+plen = bs - len(plaintext) % bs
 padding = [plen]*plen
 padding = pack('b'*plen, *padding) 
 print("Base de la clé : " + str(np.array([annee, mois, jourSemaine, jour])))
-for minIter in range(0, 60):
+for minIter in range(54, 59):
   #print(minIter)
   minute = np.int16(minIter)
   for secIter in range(60): 
@@ -37,9 +36,9 @@ for minIter in range(0, 60):
 
       #print("Test de la clé : " + str(np.frombuffer(key, dtype="int16")))
       cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
-      msg = cipher.decrypt(ciphertexte + padding)
+      msg = cipher.decrypt(plaintext + padding)
       msgLisible = "".join( chr(x) for x in msg)
-
+      #msg = iv + cipher.decrypt(plaintext)
       if "test" in msgLisible :
 
         print("ClearText trouvé :" + str(msgLisible))
